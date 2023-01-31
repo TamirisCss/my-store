b46e802f2f3d3cb699fb8a525a9ec7ccf50a2750
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2023-01-31 11:54:15 +0100

    feat: add Cart Item Component and update Product Item

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,16 +1,19 @@
 import { createContext, FunctionComponent, useState, ReactNode } from "react";
 import CartProduct from "../types/cart.types";
+import Product from "../types/product.types";
 
 interface ICartContext {
   isVisible: boolean;
   products: CartProduct[];
   toggleCart: () => void;
+  addProductToCart: (product: Product) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
   isVisible: false,
   products: [],
   toggleCart: () => {},
+  addProductToCart: () => {},
 });
 
 interface children {
@@ -19,14 +22,20 @@ interface children {
 
 const CartContextProvider: FunctionComponent<children> = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false);
-  const [products] = useState<CartProduct[]>([]);
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
   const toggleCart = () => {
     setIsVisible((prevState) => !prevState);
   };
 
+  const addProductToCart = (product: Product) => {
+    setProducts((prevState) => [...prevState, { ...product, quantity: 1 }]);
+  };
+
   return (
-    <CartContext.Provider value={{ isVisible, products, toggleCart }}>
+    <CartContext.Provider
+      value={{ isVisible, products, toggleCart, addProductToCart }}
+    >
       {children}
     </CartContext.Provider>
   );
