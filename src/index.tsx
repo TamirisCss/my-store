beb55225d6672c4d5ab3a8f74d3b2677781c5740
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2023-02-23 12:50:36 +0100

    feat: add redux-thunk, Category Reducer and its async actions

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
-import CategoryContextProvider from "./contexts/category.context";
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CategoryContextProvider>
-        <App />
-      </CategoryContextProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export type RootState = ReturnType<typeof store.getState>;
 
