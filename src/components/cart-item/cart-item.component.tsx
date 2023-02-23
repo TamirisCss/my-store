2a9c7b69e22ac98e9e9a0ad1bd59752ec8774a95
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2023-02-23 11:37:32 +0100

    feat: create actions to manipulate cart

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,7 +1,12 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent } from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { HiOutlineTrash } from "react-icons/hi";
-import { CartContext } from "../../contexts/cart.context";
+import { useDispatch } from "react-redux";
+import {
+  decreaseCartProductQuantity,
+  increaseCartProductQuantity,
+  removeProductFromCart,
+} from "../../store/reducers/cart/cart.actions";
 
 // Utilities
 import CartProduct from "../../types/cart.types";
@@ -20,22 +25,18 @@ interface CartItemProps {
 }
 
 const CartItem: FunctionComponent<CartItemProps> = ({ product }) => {
-  const {
-    removeProductFromCart,
-    increaseProductQuantity,
-    decreaseProductQuantity,
-  } = useContext(CartContext);
+  const dispatch = useDispatch();
 
   const handleRemoveClick = () => {
-    removeProductFromCart(product.id);
+    dispatch(removeProductFromCart(product.id));
   };
 
   const handleIncreaseClick = () => {
-    increaseProductQuantity(product.id);
+    dispatch(increaseCartProductQuantity(product.id));
   };
 
   const handleDecreaseClick = () => {
-    decreaseProductQuantity(product.id);
+    dispatch(decreaseCartProductQuantity(product.id));
   };
 
   return (
