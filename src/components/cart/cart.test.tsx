a913415cd2a2d3254cc446b891c556ec7b672e0f
Tamiris Cristine <tamiris.css@gmail.com>
2023-06-06 15:49:13 +0200

    test: show msg if cart is empty

--- src/components/cart/cart.test.tsx
+++ src/components/cart/cart.test.tsx
@@ -23,5 +23,19 @@ describe("Cart", () => {
     getByText("R$100");
     getByText("2");
     getByText("Total: R$200");
+    getByText(/Go to Checkout/i);
+  });
+
+  it("should not show checkout button and should show an empty message if cart is empty", () => {
+    const { getByText, queryByText } = renderWithRedux(<Cart />, {
+      preloadedState: {
+        cartReducer: {
+          products: [],
+        },
+      } as any,
+    });
+
+    getByText(/Your cart is empty!/i);
+    expect(queryByText(/Go to Checkout/i)).toBeNull();
   });
 });
